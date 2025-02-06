9cd38416e60c350b9a6534aed899a8e75c08b5f7
Eko <ekovegeance7@gmail.com>
2025-02-07 01:06:35 +0700

    feat(cli): Add functionality to specify component type when adding components and create necessary folders feat(ui): Implement Test component and update Button component structure chore(deps): Add 'open' package to package.json chore(cleanup): Remove unused remove command and button template

--- dist/bin/stoicss.js
+++ dist/bin/stoicss.js
@@ -1,3 +1,40 @@
 #!/usr/bin/env node
-import { cli } from '../src/cli/index.js';
-cli();
+import { Command } from 'commander';
+import { addComponent } from '../src/cli/commands/add.js';
+import open from 'open';
+const program = new Command();
+program
+    .name('stoicss')
+    .description('§ stoicss CLI add components and dependencies to your project')
+    .version(process.env.npm_package_version);
+//  Command add with options -b, --blocks
+program
+    .command('add <component>')
+    .description('Add a component (like button, card, etc.) to UI, blocks, skeleton.')
+    .option('-b, --blocks', 'Add to blocks')
+    .option('-s, --skeleton', 'Add to skeleton')
+    .action((component, options) => {
+    let type = 'ui'; // Default type
+    if (options.blocks) {
+        type = 'blocks';
+    }
+    else if (options.skeleton) {
+        type = 'skeleton';
+    }
+    addComponent(component, type);
+});
+// help command
+program
+    .command('help')
+    .description('Display help for command')
+    .action(() => {
+    program.outputHelp();
+});
+// repo
+program
+    .command('repo')
+    .description('Open stoicss repository on GitHub')
+    .action(() => {
+    open('https://github.com/ekovegeance/stoicss');
+});
+program.parse();
--- dist/src/cli/commands/add.js
+++ dist/src/cli/commands/add.js
@@ -1,22 +1,34 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 import chalk from "chalk";
-export function addComponent(component) {
+import { fileURLToPath } from "url";
+export function addComponent(component, type) {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    // Path template (dari folder templates)
-    const templatePath = path.join(__dirname, "../../../templates", `${component}.tsx`);
-    // Path tujuan di project pengguna (misalnya `components/`)
-    const destinationPath = path.join(process.cwd(), "components/ui", `${component}.tsx`);
-    if (fs.existsSync(templatePath)) {
-        // Buat folder jika belum ada
-        fs.mkdirSync(path.dirname(destinationPath), { recursive: true });
-        // Copy file dari templates ke project pengguna
-        fs.copyFileSync(templatePath, destinationPath);
-        console.log(chalk.blue(` Component "${component}" added to components/ui${component}.tsx`));
+    // Specify the destination folder based on component type
+    let folderPath;
+    if (type === 'blocks') {
+        folderPath = path.join(__dirname, "../../../components/blocks/");
+    }
+    else if (type === 'skeleton') {
+        folderPath = path.join(__dirname, "../../../components/skeleton/");
     }
     else {
-        console.error(chalk.red(`Component "${component}" not found in stoicss.`));
+        folderPath = path.join(__dirname, "../../../components/ui/");
+    }
+    // Create the folder if it doesn't exist
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+    }
+    // Specify the template path by type
+    const templatePath = path.join(__dirname, `../../../templates/`, type, `${component}.tsx`);
+    const destinationPath = path.join(folderPath, `${component}.tsx`);
+    // Check if the template exists and copy it to the destination folder
+    if (fs.existsSync(templatePath)) {
+        fs.copyFileSync(templatePath, destinationPath);
+        console.log(chalk.green(`Component "${component}" added to components/${type}/${component}.tsx`));
+    }
+    else {
+        console.error(chalk.red(`Component ${component} not found in stoicss/${type}.`));
     }
 }
