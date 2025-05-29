ee8a0244987fe72d65edfc63253d1bae91e68700
Eko <ekovegeance7@gmail.com>
2025-05-29 14:39:33 +0700

    github package

--- app/stocks/page.tsx
+++ app/stocks/page.tsx
@@ -5,39 +5,34 @@ import StarRating from "@/templates/stocks/star-rating";
 
 const features = [
   {
-    icon: StarRating,
-    feature: "Star Rating",
-    link: "/ui",
+    example: StarRating,
+    name: "Star Rating",
+    
     command: "npx stoicss@latest add -st star-rating",
   },
   {
-    icon: Shapes,
-    feature: "blocks",
-    link: "/blocks",
+    example: Shapes,
+    name: "blocks",
     command: "npx stoicss@latest add -bl <component>",
   },
   {
-    icon: Layers,
-    feature: "stocks",
-    link: "/stocks",
+    example: Layers,
+    name: "stocks",
     command: "npx stoicss@latest add -st <component>",
   },
   {
-    icon: Loader,
-    feature: "skeletons",
-    link: "/skeletons",
+    example: Loader,
+    name: "skeletons",
     command: "npx stoicss@latest add -sk <component>",
   },
   {
-    icon: PanelTop,
-    feature: "forms",
-    link: "/forms",
+    example: PanelTop,
+    name: "forms",
     command: "npx stoicss@latest add -fo <component>",
   },
   {
-    icon: StickyNote,
-    feature: "pages",
-    link: "/pages",
+    example: StickyNote,
+    name: "pages",
     command: "npx stoicss@latest add -pa <component>",
   },
 ];
@@ -47,13 +42,13 @@ export default function StocksPage() {
     <div className=" flex items-center justify-center w-full p-6">
       <div className="w-full mx-auto">
         <div className="grid md:grid-cols-3 rounded-lg overflow-hidden outline outline-[1px] outline-border outline-offset-[-1px]">
-          {features.map(({ feature, command, icon: Icon }) => (
-            <div key={feature} className="border p-6 -mt-px -ml-px">
+          {features.map(({ name, command, example: Example }) => (
+            <div key={name} className="border p-6 -mt-px -ml-px">
               <div className="mt-3 mb-2 flex text-center gap-2 font-bold tracking-tighter">
-                <span>{feature}</span>
+                <span>{name}</span>
               </div>
               <div className="flex items-center justify-center">
-                <Icon />
+                <Example />
               </div>
               {/* <p>{description}</p> */}
               <div className="mt-12">
